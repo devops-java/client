2c16859e357430bb1ec4f0c44bd3b44df3c0d902
rkpatra201 <patrarabindra0@gmail.com>
2018-08-20 16:56:21 +0530

    Update ClientApplication.java

--- src/main/java/com/example/client/ClientApplication.java
+++ src/main/java/com/example/client/ClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -14,12 +15,14 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 	
-	@GetMapping("/cr")
-	public String getClientResponse() {
+	@GetMapping("/cr/{id}")
+	public String getClientResponse(@PathVariable("id") String id) {
 
 		RestTemplate template = new RestTemplate();
-
-		String url="http://server:8080/sr";
+		
+		String serverName=id;
+		
+		String url="http://"+serverName+":8080/sr";
 		String response = template.getForObject(url, String.class);
 
 		return response;
